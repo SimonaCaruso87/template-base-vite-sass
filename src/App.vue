b8cdba328be9744e8bee513d79a76359535c4848
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-07-13 15:24:02 +0200

    Inizializzazione template

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+
+export default{
+  name:"App",
+  components:{
+
+  },
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<template>
+  <header>
+    HEADER
+  </header>
+
+  <main>
+    MAIN
+    <p>Francesco di Corpo</p>
+  </main>
+
+  <footer>
+    FOOTER
+  </footer>
+
+</template>
+
+<style lang="scss">
+@use "assets/scss/main" as*;
+</style>
